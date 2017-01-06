662ef446d6ce1daea45a3ac4cdb0fbc1b8f7cb0a
yuenhsi <yuenhsi93@gmail.com>
2017-01-06 10:40:51 -0800

    add autolayout test

---
+++ size-me-up/size-me-upTests/size_me_upTests.swift
@@ -0,0 +1,36 @@
+//
+//  size_me_upTests.swift
+//  size-me-upTests
+//
+//  Created by Yuen Hsi Chang on 1/6/17.
+//  Copyright © 2017 Yuen Hsi Chang. All rights reserved.
+//
+
+import XCTest
+@testable import size_me_up
+
+class size_me_upTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
