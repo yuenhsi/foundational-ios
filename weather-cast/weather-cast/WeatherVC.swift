b6c2f210e7cd689eef648149232c434dc57e28c0
yuenhsi <yuenhsi93@gmail.com>
2017-01-07 22:10:19 -0800

    connect tableView to datasource / delegate, filler methods

---
+++ weather-cast/weather-cast/WeatherVC.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  weather-cast
+//
+//  Created by Yuen Hsi Chang on 1/7/17.
+//  Copyright © 2017 Yuen Hsi Chang. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var weatherImage: UIImageView!
+    @IBOutlet weak var weatherLabel: UILabel!
+    @IBOutlet weak var weatherTableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        weatherTableView.dataSource = self
+        weatherTableView.delegate = self
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        return cell
+    }
+    
+}
+
