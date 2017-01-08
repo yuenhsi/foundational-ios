632ebfd04bbed07f48745c9c4febe9ab442fa10b
yuenhsi <yuenhsi93@gmail.com>
2017-01-07 20:47:07 -0800

    install alamofire pod

---
+++ weather-cast/Pods/Target Support Files/Pods-weather-cast/Pods-weather-cast-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_weather_cast : NSObject
+@end
+@implementation PodsDummy_Pods_weather_cast
+@end
---
+++ weather-cast/Pods/Target Support Files/Pods-weather-cast/Pods-weather-cast-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_weather_castVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_weather_castVersionString[];
+
